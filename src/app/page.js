7360086b65b6e8ada23d7c222e7f95cb7fcc6edf
Akash Kumar <akashkumarmahto2006@gmail.com>
2025-04-23 10:25:48 +0530

    remove all

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,8 @@
-'use client'
-import HeroSection from '@/components/HeroSection'
-import OurCoachingProgarm from '@/components/OurCoachingProgarm'
-import Sliders from '@/components/Sliders'
-import WhyChoose from '@/components/WhyChoose'
-import React, { useEffect, useState } from 'react'
-import LocomotiveScroll from 'locomotive-scroll';
+import React from 'react'
 
 const page = () => {
-  const [sliderPics, setSliderPics] = useState(['banner.jpg', 'banner.jpg', 'banner.jpg', 'banner.jpg'])
-  useEffect(() => {
-    const locomotiveScroll = new LocomotiveScroll();
-  
-    
-  }, [])
-  
   return (
-    <div>
-      <div className='max-w-7xl mx-auto px-4'>
-        <Sliders pics={sliderPics} />
-        <HeroSection/>
-        
-        <OurCoachingProgarm/>
-      </div>
-    </div>
+    <div>ucc</div>
   )
 }
 
